f5bf3a0680b914551ca15993a81d18a5ba15d379
HarshvardhanKh <harshvardhan.khaitan1202@gmail.com>
2026-02-16 00:34:48 +0530

    Added TargetCursor

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,18 @@ import {Skiper19} from "@/src/components/ui/stroke"
 import ImageHover from "@/src/components/common/ImageHover";
 import Navbar from "@/src/components/common/Navbar";
 import HorizontalGallery from "@/src/components/gallery/HorizontalGallery";
+import TargetCursor from "@/src/components/common/TargetCursor";
 
 export default function Home() {
   return (
     <>
+
+    <TargetCursor 
+        spinDuration={2}
+        hideDefaultCursor
+        parallaxOn
+  hoverDuration={0.2}
+/>
     <div><Navbar/></div>
     <div>
       <Skiper19 />
